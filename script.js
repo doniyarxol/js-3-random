ae64a7488cfc3694cba89e258a18dc232322b908
doniyarxol <132245867+doniyarxol@users.noreply.github.com>
2025-06-30 19:54:46 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let m = alert("Sizda hozir random sonlar chiqishni boshlaydi")
+
+
+let a = Math.floor(Math.random() * 100 ) +1
+
+
+let df = a * 2;
+
+
+let dv = a / 2;
+let u = confirm("biz random sonni 2-ga kopaytirmoqchi vo bolmoqchimiz!")
+let o = alert(`Random orqali shiqan son: ${a} edi; \n biz uni 2-ga kopaytiramiz: ${df}; \n va shu sonni 2-ga bolamiz: ${dv}`)
+
+
+
+
